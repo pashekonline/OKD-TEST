baf4c5e396748095a7502bbd32793a0a47862cd5
pashekonline <brusov@outlook.com>
2022-02-02 15:23:06 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
