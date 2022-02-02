5e944f17c08d2ddaa370818c0a3d8cb610f8ffde
pashekonline <56259474+pashekonline@users.noreply.github.com>
2022-02-02 15:25:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app.config['JSON_AS_ASCII'] = False
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return jsonify({"message":"Hello, World!"})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
